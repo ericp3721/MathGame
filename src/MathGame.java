421f7c4a9fdcf53c2ac931debceac899980e9fbd
BT_2N2_30 <anniec54@nycstudents.net>
2023-10-25 14:08:29 -0400

    sds

--- src/MathGame.java
+++ src/MathGame.java
@@ -4,15 +4,17 @@ public class MathGame {
 
     private Player player1;
     private Player player2;
+    private Player player3;
     private Player currentPlayer;
     private Player winner;
     private boolean gameOver;
     private Scanner scanner;
 
     // create MathGame object
-    public MathGame(Player player1, Player player2, Scanner scanner) {
+    public MathGame(Player player1, Player player2, Player player3, Scanner scanner) {
         this.player1 = player1;
         this.player2 = player2;
+        this.player3 = player3;
         this.scanner = scanner;
         currentPlayer = null; // will get assigned at start of game
         winner = null; // will get assigned when a Player wins
@@ -51,6 +53,7 @@ public class MathGame {
         System.out.println("Current Scores:");
         System.out.println(player1.getName() + ": " + player1.getScore());
         System.out.println(player2.getName() + ": " + player2.getScore());
+        System.out.println(player3.getName() + ": " + player3.getScore());
         System.out.println("--------------------------------------");
     }
 
@@ -58,6 +61,7 @@ public class MathGame {
     public void resetGame() {
         player1.reset(); // this method resets the player
         player2.reset();
+        player3.reset();
         gameOver = false;
         currentPlayer = null;
         winner = null;
@@ -67,11 +71,13 @@ public class MathGame {
 
     // randomly chooses one of the Player objects to be the currentPlayer
     private void chooseStartingPlayer() {
-        int randNum = (int) (Math.random() * 2) + 1;
+        int randNum = (int) (Math.random() * 3) + 1;
         if (randNum == 1) {
             currentPlayer = player1;
-        } else {
+        }else if (randNum == 2) {
             currentPlayer = player2;
+        }else{
+            currentPlayer = player3;
         }
     }
 
@@ -114,7 +120,9 @@ public class MathGame {
     private void swapPlayers() {
         if (currentPlayer == player1) {
             currentPlayer = player2;
-        } else {
+        }else if(currentPlayer == player2) {
+            currentPlayer = player3;
+        }else{
             currentPlayer = player1;
         }
     }
@@ -123,8 +131,19 @@ public class MathGame {
     private void determineWinner() {
         if (currentPlayer == player1) {
             winner = player2;
-        } else {
+            player2.incrementWinningStreak();
+            player1.resetWin();
+            player3.resetWin();
+        }else if(currentPlayer == player2) {
+            winner = player3;
+            player3.incrementWinningStreak();
+            player1.resetWin();
+            player2.resetWin();
+        }else{
             winner = player1;
+            player1.incrementWinningStreak();
+            player2.resetWin();
+            player3.resetWin();
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ public class Player {
 
     private String name;
     private int score;
+    private int winningSteak;
 
     public Player(String name) {
         this.name = name;
@@ -11,6 +12,9 @@ public class Player {
     public int getScore() {
         return score;
     }
+    public int getWinningSteak() {
+        return winningSteak;
+    }
 
     public String getName() {
         return name;
@@ -19,8 +23,14 @@ public class Player {
     public void incrementScore() {
         score++;
     }
+    public void incrementWinningStreak() {
+        winningSteak++;
+    }
 
     public void reset() {
         score = 0;
+    }
+    public void resetWin() {
+        winningSteak = 0;
     }
 }
